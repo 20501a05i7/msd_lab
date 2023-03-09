7bfdabfba3f11b86a3a4ea6708d3b62d31513811
20501a05i7 <111737274+20501a05i7@users.noreply.github.com>
2023-03-09 15:00:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var calculator = require('./cal');
+var x=50,y=10;
+console.log("add 50 and 10 is "+ calculator.add(x,y));
+console.log("sub 50 and 10 is "+ calculator.sub(x,y));
+console.log("mul 50 and 10 is "+ calculator.mul(x,y));
+console.log("div 50 and 10 is "+ calculator.div(x,y));
